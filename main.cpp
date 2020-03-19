64e2f0513b668d75f52aadded6f8bdc21a139200
matheus <medeirosmatheus0705@gmail.com>
2020-03-19 17:48:12 -0300

    kkkk

--- main.cpp
+++ main.cpp
@@ -1,34 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    std::cout<<"Hello, World!"<<std::endl;
-    for(int i=0;i<11;i++){
-        cout<<"fgggggggg=> "<<i<<endl;
-        std::cout << "Hello, World!" << std::endl;
-        std::cout << "Hello, World!" << std::endl;
-        std::cout << "Hello, World!" << std::endl;
-    }
-    std::cout << "eeeeeeeeeeeee!" << std::endl;
-    std::cout << "fffffffffff" << std::endl;
-    std::cout <<"gggggggggggg" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    for(int i=0;i<11;i++){
-        cout<<"mensagem=> "<<i<<endl;
-    }
-    cout<<"teste"<<endl;
-    std::cout <<"gggggggggggg" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    for(int i=0;i<11;i++){
-        std::cout << "Hello, World!" << std::endl;
-        std::cout << "Hello, World!" << std::endl;
-        std::cout << "Hello, World!" << std::endl;
-        cout<<"fhhhhhhhh=> "<<i<<endl;
-    }
-    cout<<"final"<<endl;
-    return 0;
+    string teste="Matheus Medeiros";
+    cout<<teste.size()<<""<<teste;
 }
