95c1281520521a03632b8cc92d37282cccaf2d12
matheus <medeirosmatheus0705@gmail.com>
2020-03-19 16:58:20 -0300

    teste branch

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@ using namespace std;
 int main()
 {
     std::cout<<"Hello, World!"<<std::endl;
+    for(int i=0;i<11;i++){
+        cout<<"mensagem=> "<<i;
+    }
+    std::cout << "eeeeeeeeeeeee!" << std::endl;
+    std::cout << "fffffffffff" << std::endl;
+    std::cout <<"gggggggggggg" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    for(int i=0;i<11;i++){
+        cout<<"mensagem=> "<<i;
+    }
     cout<<"teste"<<endl;
     return 0;
 }
